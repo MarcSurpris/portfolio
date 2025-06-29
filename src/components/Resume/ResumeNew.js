ea0b0206897f805abc495d3f36bb0f39d006533a
w3schools-test <test@w3schools.com>
2025-06-29 16:31:44 -0400

    w

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Mr.Surpris Resume.docx.pdf";
+import pdf from "../../Assets/Mr.Surpris Airbnb resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
